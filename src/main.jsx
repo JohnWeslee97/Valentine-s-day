8b16de88ae6e74730c25df23661bff84f215d40c
johnweslee92 <johnwesleejohnweslee@gmail.com>
2026-02-18 14:26:58 +0530

    fix:valentine.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import Valentine from './Components/valentine.jsx'
+import Valentine from './Components/Valentine.jsx'
 import Lastpage from './Components/Lastpage.jsx'
 
 
